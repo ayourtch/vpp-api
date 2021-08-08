851046fda83e96318eb51562d6e1501f358f3d83
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-08-08 14:08:34 +0530

    Created macro for message name and crc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
+
+use proc_macro2::TokenTree;
+
+
+#[proc_macro_derive(Message,attributes(message_name_and_crc))]
+pub fn derive_message(input:proc_macro::TokenStream) -> proc_macro::TokenStream {
+        let input = parse_macro_input!(input as DeriveInput);
+        let attribute_tokens = input.attrs[0].tokens.clone();
+        let mut token_iter = attribute_tokens.into_iter();
+        let first = token_iter.next().unwrap();
+        let ident =  match first{
+            TokenTree::Group(ref g) => {
+                let stream = g.stream().clone();
+                let mut stream_iter = stream.into_iter();
+                stream_iter.next().unwrap().to_string()
+            },
+            _ => panic!("Wrong format for message name and crc")
+        };
+        let name = input.ident;
+        let expanded = quote! {
+                 impl #name {
+                    fn get_message_name_and_crc() -> String {
+                         String::from(#ident)
+                    }
+                }
+            };
+        expanded.into()
 }
