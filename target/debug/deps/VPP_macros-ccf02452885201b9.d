79863b56c2d85ef7c277549a4c7a260ab2f97187
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-08-08 13:11:52 +0530

    Added boiler plate code with cargo

---
+++ target/debug/deps/VPP_macros-ccf02452885201b9.d
@@ -0,0 +1,5 @@
+/home/felix/LFX-Mentorship/VPP-macros/target/debug/deps/VPP_macros-ccf02452885201b9.rmeta: src/lib.rs
+
+/home/felix/LFX-Mentorship/VPP-macros/target/debug/deps/VPP_macros-ccf02452885201b9.d: src/lib.rs
+
+src/lib.rs:
