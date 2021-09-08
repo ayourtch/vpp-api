0ba330d9a63fc778364d9ae98ea7b1fa4f53a0e7
Andrew Yourtchenko <ayourtch@gmail.com>
2021-09-08 14:12:20 +0200

    Use vpp-api-{encoding|transport} from ayourtch repo rather than felixfaisal

--- src/bin/api-gen/code_gen.rs
+++ src/bin/api-gen/code_gen.rs
@@ -72,8 +72,8 @@ pub fn create_cargo_toml(packageName: &str) {
     code.push_str("typenum = \"*\" \n");
     code.push_str("bincode = \"1.2.1\" \n");
     code.push_str("serde_yaml = \"0.8\" \n");
-    code.push_str("vpp-api-encoding = {git=\"https://github.com/felixfaisal/vpp-api-encoding\", branch=\"main\" } \n");
-    code.push_str("vpp-api-transport = { git=\"https://github.com/felixfaisal/vpp-api-transport/\", branch=\"main\" } \n");
+    code.push_str("vpp-api-encoding = {git=\"https://github.com/ayourtch/vpp-api-encoding\", branch=\"main\" } \n");
+    code.push_str("vpp-api-transport = { git=\"https://github.com/ayourtch/vpp-api-transport/\", branch=\"main\" } \n");
     code.push_str("lazy_static = \"1.4.0\" \n");
     code.push_str("regex = \"1\" \n");
     code.push_str("syn ={ version= \"1.0\", features=[\"extra-traits\",\"full\"]} \n");
