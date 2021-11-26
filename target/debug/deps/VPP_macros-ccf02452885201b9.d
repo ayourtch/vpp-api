7e647338c78dbde2a447ffdc0f6c5b101e231bbc
Andrew Yourtchenko <ayourtch@gmail.com>
2021-11-26 14:21:47 +0100

    clean up the erroneously added files

--- target/debug/deps/VPP_macros-ccf02452885201b9.d
+++
@@ -1,5 +0,0 @@
-/home/felix/LFX-Mentorship/VPP-macros/target/debug/deps/VPP_macros-ccf02452885201b9.rmeta: src/lib.rs
-
-/home/felix/LFX-Mentorship/VPP-macros/target/debug/deps/VPP_macros-ccf02452885201b9.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/VPP_macros-e6230781e0946847.d
+++
@@ -1,5 +0,0 @@
-/home/felix/LFX-Mentorship/VPP-macros/target/debug/deps/VPP_macros-e6230781e0946847.rmeta: src/lib.rs
-
-/home/felix/LFX-Mentorship/VPP-macros/target/debug/deps/VPP_macros-e6230781e0946847.d: src/lib.rs
-
-src/lib.rs:
